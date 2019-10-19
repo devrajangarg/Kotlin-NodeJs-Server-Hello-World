da38eee9d4c0b724670a1cd6b13465c463bd687a
Rajan Garg <dev.rajangarg@gmail.com>
2019-10-19 18:11:20 +0530

    made improvements

--- src/kotlin/main.kt
+++ src/kotlin/main.kt
@@ -1,17 +1,18 @@
-external fun require(module:String): dynamic
+external fun require(module: String): dynamic
 
 fun main(args: Array<String>) {
 
     val port = 3000
 
-    println("Server running on https://localhost:"+port)
+    println("Server running on https://localhost:$port")
 
     val express = require("express")
     val app = express()
 
-    app.get("/", {req, res->
+    app.get("/") { req, res ->
         res.send("Hello World")
-    })
-    
+    }
+
     app.listen(port)
+
 }
